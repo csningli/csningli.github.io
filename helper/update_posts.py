5d5509d4ec9f3a832d479272574e4ed9fe8c0844
csningli <csningli@gmail.com>
2018-06-13 22:22:31 +0800

    minor update.

--- helper/update_posts.py
+++ helper/update_posts.py
@@ -60,7 +60,7 @@ def create_html(md_filename) :
 
     with open(posts_dir + "/" + md_filename, 'r') as f :
         for line in f :
-            md.append(line)
+            md.append(line.strip('\n').replace('\'', '\\\''))
             if title == "" and line.strip()[0] == '#' :
                 title = line[1:].strip()
     # print(timelabel, "-", title, "-", md)
@@ -96,7 +96,7 @@ def create_html(md_filename) :
 
         f.write("document.getElementById(\"time\").innerHTML = \"[Posted on: %s]\"\n" % timelabel)
         for line in md :
-            f.write("document.getElementById(\"post\").innerHTML += md.render(\'%s\');\n" % line.strip('\n'))
+            f.write("document.getElementById(\"post\").innerHTML += md.render(\'%s\');\n" % line)
 
         f.write(r'''</script>
         <div id="footer"></div>
